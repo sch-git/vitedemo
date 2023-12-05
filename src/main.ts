90c06ffff2a78ed2e55141edb8367208c0a0a22e
suchenghao <suchenghao@qimao.com>
2023-12-05 19:03:29 +0800

    feat: init router

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'element-plus/dist/index.css'
+import router from './router/index.ts'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
