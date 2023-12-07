ebde385ff64e9275cf94a91fa823e1cbfd69d132
suchenghao <suchenghao@qimao.com>
2023-12-07 13:38:49 +0800

    feat: container vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import Home from "../views/Home.vue";
+import Container from "../views/Container.vue";
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home
+    },
+    {
+        path: "/container",
+        name: "Container",
+        component: Container
     }
 ]
 const router = createRouter({
---
+++ src/views/Container.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
